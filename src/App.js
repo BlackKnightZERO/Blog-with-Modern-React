b6e700d76bf23cf6d47e427de15ba9967dd17801
Arif Faysal <ayon.arif.10@gmail.com>
2021-12-21 00:26:19 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
-import './App.css';
+import './Navbar';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
 
-  const title = 'Welcome to Z3R0\'s Blog'
-  const likes = 90;
-
-  const author = { name: 'Author-Name', published: '2021-03-05' } // obj directly can not be rendered
-  const isActive = true; // doesn't render
-
-  const link = 'https://google.com/';
-
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{ title }</h1>
-        <p>Liked { likes } times</p>
-        {/* <p>{ author }</p> */}
-        {/* <p>{ isActive }</p> */}
-        <p>{ 10 }</p>
-        <p>{ 'string' }</p>
-        <p>{ [1,2,3,4,5] }</p>
-        <p>{ (Math.random() * 10).toFixed(3) }</p>
-        <a href={ link } target={ '_blank' }>Google</a>
+        <Home />
       </div>
     </div>
   );
