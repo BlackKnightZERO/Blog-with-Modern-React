0186ac0b3e6dafa97e91bf30b61bff080a1df672
Arif Faysal <ayon.arif.10@gmail.com>
2021-12-20 23:59:25 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const title = 'Welcome to Z3R0\'s Blog'
+  const likes = 90;
+
+  const author = { name: 'Author-Name', published: '2021-03-05' } // obj directly can not be rendered
+  const isActive = true; // doesn't render
+
+  const link = 'https://google.com/';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>{ title }</h1>
+        <p>Liked { likes } times</p>
+        {/* <p>{ author }</p> */}
+        {/* <p>{ isActive }</p> */}
+        <p>{ 10 }</p>
+        <p>{ 'string' }</p>
+        <p>{ [1,2,3,4,5] }</p>
+        <p>{ (Math.random() * 10).toFixed(3) }</p>
+        <a href={ link } target={ '_blank' }>Google</a>
+      </div>
     </div>
   );
 }
