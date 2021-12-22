a1f8d5614a7117ad36ab6dd7ff7e1d54809a866f
Arif Faysal <ayon.arif.10@gmail.com>
2021-12-22 12:13:55 -0500

    important->json-server-cmd,resource,fetch,conditional-rendering

--- src/Home.js
+++ src/Home.js
@@ -5,12 +5,7 @@ const Home = () => {
 
     const [title, setTitle] = useState('All Blogs');
 
-    const [blogs, setBlogs] =  useState([
-        {id:1, title:'Learing Javascript', body: 'story of article...', author:'Arif'},
-        {id:2, title:'Time Management', body: 'story of article...', author:'Ayon'},
-        {id:3, title:'Mental Health', body: 'story of article...', author:'Faysal'},
-        {id:4, title:'React is Fun', body: 'story of article...', author:'Arif'},
-    ]);
+    const [blogs, setBlogs] =  useState(null);
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter( (blog) => blog.id !==id )
@@ -18,19 +13,25 @@ const Home = () => {
         setBlogs(newBlogs);
     }
 
-    // useEffect(() => {
-    //     console.log('useEffect runs on every re-render')
-    //     console.log(blogs)
-    // }, [])      //dependency array empty - therefore runs 1 time only
-
     useEffect(() => {
-        console.log('useEffect runs on every re-render')
-        console.log(blogs)
-    }, [blogs])    //dependency array runs in perspective of state
+
+        // open up json-live server to fetch resporces // npx json-server --watch DB/db.json --port 8000
+        let url = `http://localhost:8000/blogs`; 
+
+        fetch(url)                  // async await not required if used fetch-then-then
+        .then(res => res.json())    // json obj
+        .then((data) => {           // data holds array of objs of the resource(blogs)
+            console.log(data);
+            setBlogs(data);
+        })
+
+    }, []) 
     
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title={title} strMsg="list-view" handleDelete={handleDelete} />  
+            {
+               blogs && <BlogList blogs={blogs} title={title} strMsg="list-view" handleDelete={handleDelete} />  
+            }
             {/* passing as propsName={propsValue} */}
         </div>
      );
