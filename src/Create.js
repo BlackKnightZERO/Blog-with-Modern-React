3dae55c2b3c1dd573c98d3e7419414b96cdc7754
Arif Faysal <ayon.arif.10@gmail.com>
2021-12-25 23:05:12 -0500

    important & initial

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,95 @@
+import { useState } from "react";
+
 const Create = () => {
+
+    const [title, setTitle]     = useState('');
+    const [body, setBody]       = useState('');
+    const [author, setAuthor]   = useState('mario');
+    const [error, setError]     = useState(null);
+    const [success, setSuccess] = useState(false);
+    const [isPending, setIsPending] = useState(false);
+
+    const clearForm = () => {
+        setTitle('');
+        setBody('');
+        setAuthor('mario');
+        setError(null);
+        setIsPending(false);
+    }
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        setIsPending(true);
+
+        const blog = {
+            title,
+            body,
+            author
+        }
+        // console.log(blog)
+
+        let url = `http://localhost:8000/blogs`;
+
+        fetch(url, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(blog)
+        })
+        .then((res) => {
+            if( !res.ok ) {
+                setSuccess(false);
+                throw Error(`${res.url} - ${res.status} ${res.statusText}`)
+            }
+            setSuccess(true);
+            // clearForm();
+        })
+        .catch(err => {
+            setError(err.message);
+        })
+
+        setIsPending(false);
+
+    }
+
     return ( 
         <div className="create">
-            <h1>Create a New Blog</h1>
+
+            {
+                error && <div className="error">{error}<br></br></div>
+            }
+            {
+                success && <div className="success">Blog added successfully<br></br></div>
+            }
+
+            <h2>Add a New Blog</h2>
+
+            <form onSubmit={ handleSubmit }>
+
+                <label>Blog Title:</label>
+                <input 
+                    type="text" 
+                    required 
+                    value={ title }
+                    onChange={ (e) => setTitle(e.target.value) }
+                />
+
+                <label>Blog Body:</label>
+                <textarea cols="30" rows="10" required onChange={ (e) => setBody(e.target.value) } defaultValue={body}></textarea> {/* defaultValue for textarea */}
+                
+                <select onChange={ (e) => setAuthor(e.target.value) } >
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+                {
+                    isPending && <button disabled>Adding..</button>
+                }
+                {
+                    !isPending && <button >Create</button>
+                }
+
+                <p>{title} - {body} - {author}</p>
+            </form>
         </div>
      );
 }
