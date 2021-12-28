25f7e4045937bfc1524623911079d41dba2af319
Arif Faysal <ayon.arif.10@gmail.com>
2021-12-27 19:39:41 -0500

    404-page-clock-added

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,11 +1,22 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const NotFound = () => {
+
+    const [localTime, setLocalTime] = useState('')
+
+    useEffect(() => {
+        setInterval(() => {
+            setLocalTime(new Date().toLocaleTimeString())
+        }, 1000)
+    }, [])
+
     return ( 
         <div className="not-found">
             <h2>404</h2>
             <p>The page that are looking for is Not Found</p>
             <Link to="/"> {'<'} go back</Link>
+            <h1>{ localTime }</h1>
         </div>
      );
 }
